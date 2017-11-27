90cdd029fc4386805fda77a4356e9491ed884e98
Shyscarecrow <orlando2004@inbox.ru>
2017-11-27 20:38:58 +0300

    Add designs.js

---
+++ designs.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+var color, height, width;
+color = "black";
+$("#colorPicker").on("change", function() {
+	color = $(this).val();
+});
+
+$("#input_height").on("change", function() {
+	height = $(this).val();
+});
+
+$("#input_width").on("change", function() {
+	width = $(this).val();
+});
+
+$(":submit").on("click", function makeGrid(e) {
+	e.preventDefault();
+	for (var i = 1; i <= height; i++) {
+		$("table").append("<tr></tr>");
+	};
+	for (var j = 1; j <= width; j++) {
+		$("tr").append("<td></td>");
+	};
+	$("td").click(function(){
+		$(this).css("background-color", color);
+	});
+	$("td").dblclick(function(){
+		$(this).css("background-color", "white");
+	});
+});
+});
